2b1354736260e8c5fc625440d46471826d716fa0
Châu Ngọc Sơn <ngocson877469@gmail.com>
2026-09-08 14:58:11 +0700

    Remove unused delivery skeleton

--- src/main/java/com/qs/Backend/modules/notification/delivery/repository/DeliveryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.qs.Backend.modules.notification.delivery.repository;
-
-import com.qs.Backend.modules.notification.delivery.entity.Delivery;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DeliveryRepository extends JpaRepository<Delivery, Long> {
-}

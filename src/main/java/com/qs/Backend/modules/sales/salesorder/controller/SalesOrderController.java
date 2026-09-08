866c987defd6aa935d70c9898e5b8e44cf42024e
Châu Ngọc Sơn <ngocson877469@gmail.com>
2026-09-08 10:16:29 +0700

    Normalize sales order route

--- src/main/java/com/qs/Backend/modules/sales/salesorder/controller/SalesOrderController.java
+++ src/main/java/com/qs/Backend/modules/sales/salesorder/controller/SalesOrderController.java
@@ -11,7 +11,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/sales-orders")
+@RequestMapping("/sales-orders")
 @RequiredArgsConstructor
 public class SalesOrderController {
 
